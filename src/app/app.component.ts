4629dccbedd8be6a56a51bc053f300c5e668277f
alexanderavva <alexanderavva@gmail.com>
2023-01-19 16:35:15 +0300

    eee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {User} from "./models/user";
+import {AccountService} from "./services/user/account.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  user?: User  ;
   title = 'dbmsp';
+
+  constructor(private accountService: AccountService) {
+    this.accountService.user.subscribe(x => this.user = x);
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
+import { CPgSettingComponent } from './components/pg/cpg-setting/cpg-setting.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
+import { CDbmsListComponent } from './components/pg/cdbms-list/cdbms-list.component';
+import { CLoginComponentComponent } from './components/admin/clogin-component/clogin-component.component';
+import {JwtInterceptor} from "./models/jwt.interceptor";
+import {ErrorInterceptor} from "./services/user/error.interceptor";
+import {AlertComponent} from "./services/user/alertcomponent/alert.component";
+import {DatabaseListComponent } from './components/admin/databaselist/database-list/database-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CPgSettingComponent,
+    CDbmsListComponent,
+    CLoginComponentComponent,
+    AlertComponent,
+    DatabaseListComponent
   ],
   imports: [
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [ { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }],/**/
   bootstrap: [AppComponent]
 })
 export class AppModule { }
